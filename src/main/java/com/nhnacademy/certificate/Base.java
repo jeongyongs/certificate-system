459631bf7b30033bcc6f04c4051c048fb1c36983
Jeongyong Lee <hua9378@gmail.com>
2024-02-16 11:16:12 +0900

    스프링 웹 MVC 설정

---
+++ src/main/java/com/nhnacademy/certificate/Base.java
@@ -0,0 +1,5 @@
+package com.nhnacademy.certificate;
+
+public interface Base {
+    // 프로젝트 베이스 패키지 경로를 잡기 위한 마커 인터페이스
+}
---
+++ src/main/java/com/nhnacademy/certificate/controller/ControllerBase.java
@@ -0,0 +1,5 @@
+package com.nhnacademy.certificate.controller;
+
+public interface ControllerBase {
+    // 컨트롤러 베이스 패키지 경로를 잡기 위한 마커 인터페이스
+}
